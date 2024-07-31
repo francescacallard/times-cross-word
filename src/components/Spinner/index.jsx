8cad155dc338c043bf32f1efdeab1a691641cfc6
francescacallard <francescalily@outlook.com>
2024-07-31 09:24:42 +0100

    getting the generate spacing correct and grid numbers

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -4,6 +4,6 @@ import spinnerAnimation from '../../assets/spinner.json'
 
 export const Spinner = () => {
   return (
-    <Lottie animationData={spinnerAnimation} loop={true} autoplay={true} style={{ width: 16, height: 16 }} />
+    <Lottie animationData={spinnerAnimation} loop={true} autoplay={true} style={{ width: 16, height: 16, paddingRight: 8, }} />
   )
 }
