0e1ef73df0aefb02de47e8626adae1fb959d9bea
francescacallard <francescalily@outlook.com>
2024-09-19 15:31:41 +0100

    new correct branch for personlised crossword

--- src/routes/Index/index.tsx
+++ src/routes/Index/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { useIntl } from 'react-intl'
 import { GenerateCrossword } from 'components/GenerateCrossword'
 import { PaintGrid } from 'components/PaintGrid'
 import 'App.css'
 
 const Index: React.FC = () => {
-  const { formatMessage } = useIntl()
 
   return (
     <div className="App">
